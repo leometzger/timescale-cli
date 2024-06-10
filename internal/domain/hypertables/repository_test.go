9e6c50d73eb27f7d36138c80b456e838c119f23e
Leonardo Metzger <leonardo.metzger@outlook.com>
2024-06-10 08:08:27 -0300

    chore: add tests to hypertables repository

--- internal/domain/hypertables/repository_test.go
+++ internal/domain/hypertables/repository_test.go
@@ -4,8 +4,9 @@ import (
 	"log/slog"
 	"testing"
 
+	"github.com/leometzger/timescale-cli/internal/domain"
 	"github.com/leometzger/timescale-cli/testlib"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestGetHypertableInformationFromTimescale(t *testing.T) {
@@ -15,7 +16,40 @@ func TestGetHypertableInformationFromTimescale(t *testing.T) {
 
 	hypertables, err := repository.GetHypertables(&filter)
 
-	assert.Nil(t, err)
-	assert.Equal(t, 1, len(hypertables))
-	assert.Equal(t, "metrics", hypertables[0].HypertableName)
+	require.Nil(t, err)
+	require.Equal(t, 1, len(hypertables))
+	require.Equal(t, "metrics", hypertables[0].HypertableName)
+}
+
+func TestGetHypertableInformationFromTimescaleWithNameFilter(t *testing.T) {
+	conn := testlib.GetConnection()
+	repository := NewHypertablesRepository(conn, slog.Default().WithGroup("hypertables"))
+	filter := HypertablesFilter{Name: "metricsaa"}
+
+	hypertables, err := repository.GetHypertables(&filter)
+
+	require.Nil(t, err)
+	require.Equal(t, 0, len(hypertables))
+}
+
+func TestGetHypertableInformationFromTimescaleWithCompressFilter(t *testing.T) {
+	conn := testlib.GetConnection()
+	repository := NewHypertablesRepository(conn, slog.Default().WithGroup("hypertables"))
+	filter := HypertablesFilter{Compressed: domain.OptionFlagTrue}
+
+	hypertables, err := repository.GetHypertables(&filter)
+
+	require.Nil(t, err)
+	require.Equal(t, 0, len(hypertables))
+}
+
+func TestGetHypertableInformationFromTimescaleWithCompoundFilter(t *testing.T) {
+	conn := testlib.GetConnection()
+	repository := NewHypertablesRepository(conn, slog.Default().WithGroup("hypertables"))
+	filter := HypertablesFilter{Name: "metrics", Compressed: domain.OptionFlagFalse}
+
+	hypertables, err := repository.GetHypertables(&filter)
+
+	require.Nil(t, err)
+	require.Equal(t, 1, len(hypertables))
 }
